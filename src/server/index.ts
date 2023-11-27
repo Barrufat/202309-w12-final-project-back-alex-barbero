02cdd7f5c1973e34f2be7f5810e03adae8c5b681
Alex Barbero <barrufat@hotmail.com>
2023-11-27 17:04:54 +0100

    Add cors configurations

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,17 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import app from "./app.js";
+import cors from "cors";
 
 app.use(morgan("dev"));
-app.use(express());
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      "https://alex-barbero-202309-bcn-front.netlify.app/",
+      "http://localhost:4000",
+    ],
+  }),
+);
