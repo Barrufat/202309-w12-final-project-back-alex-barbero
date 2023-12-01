02904954a69fe400ff8a9f8a59374893645010ba
Alex Barbero <barrufat@hotmail.com>
2023-12-01 12:48:53 +0100

    Fix coverage configurations

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,12 +5,12 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
-  coveragePathIgnorePatterns: [
-    "src/**/*.ts",
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!src/index.ts",
+    "!src/server/app.ts",
+    "!src/database/index.ts",
+    "!src/setupTest.ts",
     "!**/node_modules/**",
-    "!**/src/index.ts",
-    "!**src/setupTest.ts",
-    "!**src/database/index.ts",
-    "!**src/server/app.ts",
   ],
 };
