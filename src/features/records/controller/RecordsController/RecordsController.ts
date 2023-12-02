a7677c347dc913a9cb4089c80b109fb59c106045
Alex Barbero <barrufat@hotmail.com>
2023-12-02 23:59:58 +0100

    Add deleteRecord to RecordsController

--- src/features/records/controller/RecordsController/RecordsController.ts
+++ src/features/records/controller/RecordsController/RecordsController.ts
@@ -1,5 +1,6 @@
 import type { Response, Request } from "express";
 import type RecordsMongooseRepository from "../../repository/RecordsMongooseRepository";
+import { type ByRecordId } from "../../types";
 
 class RecordsController {
   constructor(private readonly recordsRepository: RecordsMongooseRepository) {}
@@ -8,6 +9,19 @@ class RecordsController {
     const records = await this.recordsRepository.getRecords();
 
     res.status(200).json({ records });
+  };
+
+  deleteRecordById = async (req: ByRecordId, res: Response): Promise<void> => {
+    const { recordId } = req.params;
+
+    const record = await this.recordsRepository.deleteRecord(recordId);
+    const deleteMessage = `${record.bandName} ${record.albumName} successfully deleted`;
+
+    try {
+      res.status(200).json({ message: deleteMessage });
+    } catch {
+      res.status(400).json({ error: "current thing not found" });
+    }
   };
 }
 
