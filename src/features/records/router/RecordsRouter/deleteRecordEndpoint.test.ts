e0bff82955caf751b570758df9bdbb8ab3afd15c
Alex Barbero <barrufat@hotmail.com>
2023-12-06 22:43:06 +0100

    Refactor deleteRecord and generalError tests

--- src/features/records/router/RecordsRouter/deleteRecordEndpoint.test.ts
+++ src/features/records/router/RecordsRouter/deleteRecordEndpoint.test.ts
@@ -1,33 +1,22 @@
 import request from "supertest";
 import "../../../../server/index";
 import app from "../../../../server/app";
-import { type RecordStructureWithId } from "../../types";
+import { recordsMock } from "../../mocks/recordsMock";
 
 describe("Given a DELETE /records endpoint", () => {
   describe("When it recives a request with and id", () => {
     test("Then it should respond with the text 'Ejtopa Fui a la orilla der rio y vi ke taba mu chola successfully deleted' ", async () => {
-      const expectedGetStatusCode = 200;
-      const requestedGetPath = "/records";
-
-      const responseGet = await request(app)
-        .get(requestedGetPath)
-        .expect(expectedGetStatusCode);
-
-      const responseGetBody = responseGet.body as {
-        records: RecordStructureWithId[];
-      };
-
       const expectedStatusCode = 200;
-      const requestedPath = `/records/${responseGetBody.records[0]._id}`;
+      const requestedPath = `/records/65627f915a1ecd043c5d123a`;
 
       const response = await request(app)
         .delete(requestedPath)
         .expect(expectedStatusCode);
 
-      const responseBody = response.body as { message: string };
+      const responseBody = (await response.body) as { message: string };
 
       expect(responseBody.message).toBe(
-        `${responseGetBody.records[0].bandName} ${responseGetBody.records[0].albumName} successfully deleted`,
+        `${recordsMock[0].bandName} ${recordsMock[0].albumName} successfully deleted`,
       );
     });
   });
