166a5cd8a3ff23245f02fa96ceb6b3964b60f628
Alex Barbero <barrufat@hotmail.com>
2023-12-08 13:14:46 +0100

    Add getRecordById to RecordsRepository

--- src/features/records/repository/RecordsMongooseRepository.ts
+++ src/features/records/repository/RecordsMongooseRepository.ts
@@ -4,24 +4,24 @@ import { type RecordsRepository } from "./types";
 
 class RecordsMongooseRepository implements RecordsRepository {
   async getRecords(): Promise<RecordStructure[]> {
-    const records = await Record.find();
+    const records = await Record.find().sort({ _id: -1 }).limit(10);
 
     return records;
   }
 
   async deleteRecord(recordId: string): Promise<RecordStructure> {
     const deletedRecord = await Record.findByIdAndDelete(recordId);
-
-    await Record.findByIdAndDelete(recordId);
-
     return deletedRecord!;
   }
 
-  public async createRecord(
-    record: RecordStructure,
-  ): Promise<RecordStructureWithId> {
+  async createRecord(record: RecordStructure): Promise<RecordStructureWithId> {
     const createdRecord = await Record.create(record);
     return createdRecord;
+  }
+
+  async getRecordById(recordId: string): Promise<RecordStructure> {
+    const record = await Record.findById(recordId);
+    return record!;
   }
 }
 
